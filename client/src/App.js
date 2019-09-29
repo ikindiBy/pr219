ad2cf9c1ef0692385dfc18e1ed41d99a315d2a6b
ikindiBy <ikindi@mail.ru>
2019-09-29 12:24:44 +0300

    change serners response

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -12,23 +11,23 @@ function App() {
     });
   }, []);
 
-  return (
-    user && (
-      <div className="App">
-        <header className="App-header">
-          <img src={user.user.avatar_url} className="App-logo" alt="logo" />
-          <p>{user.user.login}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Hello deploed React App!
-          </a>
-        </header>
-      </div>
-    )
+  return user ? (
+    <div className="App">
+      <header className="App-header">
+        <img src={user.user.avatar_url} className="App-logo" alt="logo" />
+        <p>{user.user.login}</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello deploed React App!
+        </a>
+      </header>
+    </div>
+  ) : (
+    <h2>No no no !</h2>
   );
 }
 
--- index.js
+++ index.js
@@ -10,27 +10,27 @@ const PORT = process.env.PORT || 80;
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.end(`
-  <div>
-    <nav>
-        <ul>
-            <li>
-                <a href="/">Home</a>
-            </li>
-            <li>
-                <a href="/about">About</a>
-            </li>
-        </ul>
-    </nav>
-    <h2>Home page</h2>
-  </div>
-  `);
-});
+// app.get("/", (req, res) => {
+//   res.end(`
+//   <div>
+//     <nav>
+//         <ul>
+//             <li>
+//                 <a href="/">Home</a>
+//             </li>
+//             <li>
+//                 <a href="/about">About</a>
+//             </li>
+//         </ul>
+//     </nav>
+//     <h2>Home page</h2>
+//   </div>
+//   `);
+// });
 
-app.get("/about", (req, res) => {
-  res.end(`<div><h2>About</h2></div>`);
-});
+// app.get("/about", (req, res) => {
+//   res.end(`<div><h2>About</h2></div>`);
+// });
 
 app.get("/api", (req, res) => {
   const user = req.query.user || "ikindiBy";
